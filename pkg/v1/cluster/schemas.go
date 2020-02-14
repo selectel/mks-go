30530306e5c02de22ce006ecd2a1dd38a0af0ba4
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2020-02-14 10:44:56 +0300

    Cluster V1 - add cluster Get method (#4)
    * Cluster V1 - add cluster Get method
    
    Add Get method with basic view structure.
    Provide tests and doc example.
    
    * Add "enable_autorepair" field in cluster view
    
    Add "enable_autorepair" field in cluster view structure.
    Update testing fuxtures.
    
    * Update tests
    
    Rename testGetClusterResponse variable to expectedGetClusterResponse
    in cluster/testing package and update tests.

--- pkg/v1/cluster/schemas.go
+++ pkg/v1/cluster/schemas.go
@@ -1,1 +1,61 @@
 package cluster
+
+import "time"
+
+// View represents an unmarshalled cluster body from an API response.
+type View struct {
+	// ID is the identifier of the cluster.
+	ID string `json:"id"`
+
+	// CreatedAt is the timestamp in UTC timezone of when the cluster has been created.
+	CreatedAt time.Time `json:"created_at"`
+
+	// UpdatedAt is the timestamp in UTC timezone of when the cluster has been updated.
+	UpdatedAt time.Time `json:"updated_at"`
+
+	// Name represents the name of the cluster.
+	Name string `json:"name"`
+
+	// Status represents current status of the cluster.
+	Status string `json:"status"`
+
+	// ProjectID contains reference to the project of the cluster.
+	ProjectID string `json:"project_id"`
+
+	// NetworkID contains reference to the network of the cluster.
+	NetworkID string `json:"network_id"`
+
+	// SubnetID contains reference to the subnet of the cluster.
+	SubnetID string `json:"subnet_id"`
+
+	// KubeAPIIP represents the IP of the Kubernetes API.
+	KubeAPIIP string `json:"kube_api_ip"`
+
+	// KubeVersion represents the current Kubernetes version of the cluster.
+	KubeVersion string `json:"kube_version"`
+
+	// Region represents the region of where the cluster is located.
+	Region string `json:"region"`
+
+	// AdditionalSoftware represents information about additional software installed in the cluster.
+	AdditionalSoftware map[string]interface{} `json:"additional_software"`
+
+	// PKITreeUpdatedAt represents the timestamp in UTC timezone of when the PKI-tree of the cluster
+	// has been updated.
+	PKITreeUpdatedAt time.Time `json:"pki_tree_updated_at"`
+
+	// MaintenanceWindowStart represents UTC time in "hh:mm:ss" format of when the cluster will start its
+	// maintenance tasks.
+	MaintenanceWindowStart string `json:"maintenance_window_start"`
+
+	// MaintenanceWindowEnd represents UTC time in "hh:mm:ss" format of when the cluster will end its
+	// maintenance tasks.
+	MaintenanceWindowEnd string `json:"maintenance_window_end"`
+
+	// MaintenanceLastStart is the timestamp in UTC timezone of the last cluster maintenance start.
+	MaintenanceLastStart time.Time `json:"maintenance_last_start"`
+
+	// EnableAutorepair reflects if worker nodes are allowed to be reinstalled automatically
+	// in case of their unavailability or unhealthiness.
+	EnableAutorepair bool `json:"enable_autorepair"`
+}
--- pkg/v1/cluster/testing/fixtures.go
+++ pkg/v1/cluster/testing/fixtures.go
@@ -1,1 +1,64 @@
 package testing
+
+import (
+	"time"
+
+	"github.com/selectel/mks-go/pkg/v1/cluster"
+)
+
+// testGetClusterResponseRaw represents a raw response from the Get request.
+const testGetClusterResponseRaw = `
+{
+    "cluster": {
+        "additional_software": null,
+        "created_at": "2020-02-13T09:18:32.05753Z",
+        "enable_autorepair": true,
+        "id": "dbe7559b-55d8-4f65-9230-6a22b985ff73",
+        "kube_api_ip": "203.0.113.101",
+        "kube_version": "1.15.7",
+        "maintenance_last_start": "2020-02-13T09:18:32.05753Z",
+        "maintenance_window_end": "03:00:00",
+        "maintenance_window_start": "01:00:00",
+        "name": "test-cluster",
+        "network_id": "74a591be-6be7-4abc-d30f-1614c0f9721c",
+        "pki_tree_updated_at": "2020-02-13T09:18:32.05753Z",
+        "project_id": "65044a03bede4fd0a77e5a4c882e3059",
+        "region": "ru-1",
+        "status": "ACTIVE",
+        "subnet_id": "c872541d-2d83-419f-841d-8288201b8fb9",
+        "updated_at": "2020-02-13T09:18:32.05753Z"
+    }
+}
+`
+
+var clusterResponseTimestamp, _ = time.Parse(time.RFC3339, "2020-02-13T09:18:32.05753Z")
+
+// expectedGetClusterResponse represents an unmarshalled testGetClusterResponseRaw.
+var expectedGetClusterResponse = &cluster.View{
+	ID:                     "dbe7559b-55d8-4f65-9230-6a22b985ff73",
+	CreatedAt:              clusterResponseTimestamp,
+	UpdatedAt:              clusterResponseTimestamp,
+	Name:                   "test-cluster",
+	Status:                 "ACTIVE",
+	ProjectID:              "65044a03bede4fd0a77e5a4c882e3059",
+	NetworkID:              "74a591be-6be7-4abc-d30f-1614c0f9721c",
+	SubnetID:               "c872541d-2d83-419f-841d-8288201b8fb9",
+	KubeAPIIP:              "203.0.113.101",
+	KubeVersion:            "1.15.7",
+	Region:                 "ru-1",
+	AdditionalSoftware:     nil,
+	PKITreeUpdatedAt:       clusterResponseTimestamp,
+	MaintenanceWindowStart: "01:00:00",
+	MaintenanceWindowEnd:   "03:00:00",
+	MaintenanceLastStart:   clusterResponseTimestamp,
+	EnableAutorepair:       true,
+}
+
+// testGetClusterInvalidResponseRaw represents a raw invalid response with a single cluster.
+const testGetClusterInvalidResponseRaw = `
+{
+    "cluster": {
+        "id": "dbe7559b-55d8-4f65-9230-6a22b985ff74",
+    }
+}
+`
