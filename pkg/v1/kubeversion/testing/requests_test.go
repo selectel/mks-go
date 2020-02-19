585f00a7c2d5aeb1e24fcddeb4b9a59a6a3b2810
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2020-02-19 13:35:02 +0300

    Add kubeversion List method
    Add method to list all supported Kubernetes versions.
    Provide tests and doc example.

--- pkg/v1/kubeversion/testing/requests_test.go
+++ pkg/v1/kubeversion/testing/requests_test.go
@@ -1,1 +1,160 @@
 package testing
+
+import (
+	"context"
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/selectel/mks-go/pkg/testutils"
+	v1 "github.com/selectel/mks-go/pkg/v1"
+	"github.com/selectel/mks-go/pkg/v1/kubeversion"
+)
+
+func TestListKubeVersions(t *testing.T) {
+	endpointCalled := false
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+
+	testutils.HandleReqWithoutBody(t, &testutils.HandleReqOpts{
+		Mux:         testEnv.Mux,
+		URL:         "/v1/kubeversions",
+		RawResponse: testListKubeVersionsResponseRaw,
+		Method:      http.MethodGet,
+		Status:      http.StatusOK,
+		CallFlag:    &endpointCalled,
+	})
+
+	ctx := context.Background()
+	testClient := &v1.ServiceClient{
+		HTTPClient: &http.Client{},
+		TokenID:    testutils.TokenID,
+		Endpoint:   testEnv.Server.URL + "/v1",
+		UserAgent:  testutils.UserAgent,
+	}
+
+	actual, httpResponse, err := kubeversion.List(ctx, testClient)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if httpResponse == nil {
+		t.Fatal("expected an HTTP response from the List method")
+	}
+	if httpResponse.StatusCode != http.StatusOK {
+		t.Fatalf("expected %d status in the HTTP response, but got %d",
+			http.StatusOK, httpResponse.StatusCode)
+	}
+	if !reflect.DeepEqual(expectedListKubeVersionsResponse, actual) {
+		t.Fatalf("expected %#v, but got %#v", expectedListKubeVersionsResponse, actual)
+	}
+}
+
+func TestListKubeVersionsHTTPError(t *testing.T) {
+	endpointCalled := false
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+
+	testutils.HandleReqWithoutBody(t, &testutils.HandleReqOpts{
+		Mux:         testEnv.Mux,
+		URL:         "/v1/kubeversions",
+		RawResponse: testErrGenericResponseRaw,
+		Method:      http.MethodGet,
+		Status:      http.StatusBadGateway,
+		CallFlag:    &endpointCalled,
+	})
+
+	ctx := context.Background()
+	testClient := &v1.ServiceClient{
+		HTTPClient: &http.Client{},
+		TokenID:    testutils.TokenID,
+		Endpoint:   testEnv.Server.URL + "/v1",
+		UserAgent:  testutils.UserAgent,
+	}
+
+	actual, httpResponse, err := kubeversion.List(ctx, testClient)
+
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if actual != nil {
+		t.Fatal("expected no Kubernetes versions from the List method")
+	}
+	if httpResponse == nil {
+		t.Fatal("expected an HTTP response from the List method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the List method")
+	}
+	if httpResponse.StatusCode != http.StatusBadGateway {
+		t.Fatalf("expected %d status in the HTTP response, but got %d",
+			http.StatusBadGateway, httpResponse.StatusCode)
+	}
+}
+
+func TestListKubeVersionsTimeoutError(t *testing.T) {
+	testEnv := testutils.SetupTestEnv()
+	testEnv.Server.Close()
+	defer testEnv.TearDownTestEnv()
+
+	ctx := context.Background()
+	testClient := &v1.ServiceClient{
+		HTTPClient: &http.Client{},
+		TokenID:    testutils.TokenID,
+		Endpoint:   testEnv.Server.URL + "/v1",
+		UserAgent:  testutils.UserAgent,
+	}
+
+	actual, httpResponse, err := kubeversion.List(ctx, testClient)
+
+	if actual != nil {
+		t.Fatal("expected no Kubernetes versions from the List method")
+	}
+	if httpResponse != nil {
+		t.Fatal("expected no HTTP response from the List method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the List method")
+	}
+}
+
+func TestListKubeVersionsUnmarshallError(t *testing.T) {
+	endpointCalled := false
+	testEnv := testutils.SetupTestEnv()
+	defer testEnv.TearDownTestEnv()
+
+	testutils.HandleReqWithoutBody(t, &testutils.HandleReqOpts{
+		Mux:         testEnv.Mux,
+		URL:         "/v1/kubeversions",
+		RawResponse: testManyKubeVersionsInvalidResponseRaw,
+		Method:      http.MethodGet,
+		Status:      http.StatusOK,
+		CallFlag:    &endpointCalled,
+	})
+
+	ctx := context.Background()
+	testClient := &v1.ServiceClient{
+		HTTPClient: &http.Client{},
+		TokenID:    testutils.TokenID,
+		Endpoint:   testEnv.Server.URL + "/v1",
+		UserAgent:  testutils.UserAgent,
+	}
+
+	actual, httpResponse, err := kubeversion.List(ctx, testClient)
+
+	if !endpointCalled {
+		t.Fatal("endpoint wasn't called")
+	}
+	if actual != nil {
+		t.Fatal("expected no Kubernetes versions from the List method")
+	}
+	if httpResponse == nil {
+		t.Fatal("expected an HTTP response from the List method")
+	}
+	if err == nil {
+		t.Fatal("expected error from the List method")
+	}
+}
