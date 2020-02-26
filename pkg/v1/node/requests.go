6becce35e2d592dfd1c5713cae8d4d0de9a2e668
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2020-02-26 11:41:51 +0300

    Cluster V1 - add node Get method
    Add method to get a single node of a cluster nodegroup.
    Provide tests and doc example.

--- pkg/v1/node/requests.go
+++ pkg/v1/node/requests.go
@@ -1,1 +1,32 @@
 package node
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	v1 "github.com/selectel/mks-go/pkg/v1"
+)
+
+// Get returns a node of a cluster nodegroup by its id.
+func Get(ctx context.Context, client *v1.ServiceClient, clusterID, nodegroupID, nodeID string) (*View, *v1.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, v1.ResourceURLCluster, clusterID, v1.ResourceURLNodegroup, nodegroupID, nodeID}, "/")
+	responseResult, err := client.DoRequest(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+	if responseResult.Err != nil {
+		return nil, responseResult, responseResult.Err
+	}
+
+	// Extract a node to the response body.
+	var result struct {
+		Node *View `json:"node"`
+	}
+	err = responseResult.ExtractResult(&result)
+	if err != nil {
+		return nil, responseResult, err
+	}
+
+	return result.Node, responseResult, err
+}
--- pkg/v1/node/testing/fixtures.go
+++ pkg/v1/node/testing/fixtures.go
@@ -1,1 +1,45 @@
 package testing
+
+import (
+	"time"
+
+	"github.com/selectel/mks-go/pkg/v1/node"
+)
+
+// testGetNodeResponseRaw represents a raw response from the Get request.
+const testGetNodeResponseRaw = `
+{
+    "node": {
+        "created_at": "2020-02-25T08:49:21.780542Z",
+        "hostname": "test-cluster-node-gap1g",
+        "id": "203d0f8c-547d-48a7-98ed-3075254b8d4a",
+        "ip": "198.51.100.11",
+        "nodegroup_id": "f174b65d-442a-4423-aaf7-5654789b8a9d",
+        "updated_at": "2020-02-25T08:49:21.780542Z"
+    }
+}
+`
+
+var nodeResponseTimestamp, _ = time.Parse(time.RFC3339, "2020-02-25T08:49:21.780542Z")
+
+// expectedGetNodeResponse represents an unmarshalled testGetNodeResponseRaw.
+var expectedGetNodeResponse = &node.View{
+	ID:          "203d0f8c-547d-48a7-98ed-3075254b8d4a",
+	CreatedAt:   &nodeResponseTimestamp,
+	UpdatedAt:   &nodeResponseTimestamp,
+	Hostname:    "test-cluster-node-gap1g",
+	IP:          "198.51.100.11",
+	NodegroupID: "f174b65d-442a-4423-aaf7-5654789b8a9d",
+}
+
+// testSingleNodeInvalidResponseRaw represents a raw invalid response with a single node.
+const testSingleNodeInvalidResponseRaw = `
+{
+    "node": {
+        "id": "203d0f8c-547d-48a7-98ed-3075254b8d4a",
+    }
+}
+`
+
+// testErrGenericResponseRaw represents a raw response with an error in the generic format.
+const testErrGenericResponseRaw = `{"error":{"message":"bad gateway"}}`
