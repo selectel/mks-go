59d413b76db4fd3c511b3f4ab3634131c3ca8c1d
Sergei Kolyshkin <kolyshkin.sa@gmail.com>
2020-02-26 12:28:06 +0300

    Cluster V1 - add node Reinstall method
    Add method to reinstall a single node of a cluster nodegroup.
    Provide tests and doc example.

--- pkg/v1/node/requests.go
+++ pkg/v1/node/requests.go
@@ -30,3 +30,17 @@ func Get(ctx context.Context, client *v1.ServiceClient, clusterID, nodegroupID, 
 
 	return result.Node, responseResult, err
 }
+
+// Reinstall requests to make reinstall of a single node of a cluster nodegroup by its id.
+func Reinstall(ctx context.Context, client *v1.ServiceClient, clusterID, nodegroupID, nodeID string) (*v1.ResponseResult, error) {
+	url := strings.Join([]string{client.Endpoint, v1.ResourceURLCluster, clusterID, v1.ResourceURLNodegroup, nodegroupID, nodeID, v1.ResourceURLReinstall}, "/")
+	responseResult, err := client.DoRequest(ctx, http.MethodPost, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	if responseResult.Err != nil {
+		err = responseResult.Err
+	}
+
+	return responseResult, err
+}
