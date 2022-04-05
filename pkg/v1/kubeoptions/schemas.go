77a449d16d018ecafcee0edcfcfd770200a80963
Kulakov Ilya <kulakov@selectel.ru>
2022-04-05 19:33:51 +0300

    remove redundant nolint directives; turn off goerr113 and tagliatelle linters

--- pkg/v1/kubeoptions/schemas.go
+++ pkg/v1/kubeoptions/schemas.go
@@ -3,8 +3,8 @@ package kubeoptions
 // View represents list of feature-gates/admission-controllers by kubernetes version.
 type View struct {
 	// KubeVersion represents the Kubernetes minor version in format: "X.Y".
-	KubeVersion string `json:"KubeVersionMinor"` //nolint:tagliatelle
+	KubeVersion string `json:"KubeVersionMinor"`
 
 	// Names represents list of feature-gate names.
-	Names []string `json:"Names"` //nolint:tagliatelle
+	Names []string `json:"Names"`
 }
